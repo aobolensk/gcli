a284f824b1c36bfadb3161a278b5f1dea8a888a4
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-28 17:30:38 +0300

    (#8) Extract watchers count, owner and last update time

--- main.go
+++ main.go
@@ -33,11 +33,14 @@ func process(args []string) {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		fmt.Printf("Link: %s -> %d ⭐; %d ⑂; %d ❗\n",
+		fmt.Printf("Link: %s -> %d ⭐; %d ⑂; %d ❗; %d 👁️\n",
 			resp["html_url"].(string),
 			uint(resp["stargazers_count"].(float64)),
 			uint(resp["forks"].(float64)),
-			uint(resp["open_issues"].(float64)))
+			uint(resp["open_issues"].(float64)),
+			uint(resp["watchers"].(float64)))
+		fmt.Println("Owner: " + resp["owner"].(map[string]interface{})["html_url"].(string))
+		fmt.Println("Last update: " + resp["updated_at"].(string))
 	case "issue":
 		if len(args) == 1 {
 			// Get list of opened issues
