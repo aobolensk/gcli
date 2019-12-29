bce702fe36117caa4a133a75b4612bbbc70b98fe
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-29 12:59:55 +0300

    (#16) Introduce issue.go

--- main.go
+++ main.go
@@ -36,65 +36,10 @@ func process(args []string) {
 		}
 	case "issue":
 		if len(args) == 1 {
-			// Get list of opened issues
-			fmt.Println("List of opened issues for " + origin + ":")
-			for page := 1; ; page++ {
-				resp, err := queryList(
-					"GET",
-					"https://api.github.com/repos/"+origin+"/issues?per_page=100&page="+
-						strconv.Itoa(page))
-				if err != nil {
-					fmt.Fprintln(os.Stderr, err)
-					os.Exit(1)
-				}
-				if len(resp) == 0 {
-					break
-				}
-				for _, issue := range resp {
-					link := issue["html_url"].(string)
-					if strings.Contains(link, "issues") {
-						fmt.Printf("%-*.*s > %s\n", 50, 50, issue["title"].(string), link)
-					}
-				}
-				if err != nil {
-					fmt.Fprintln(os.Stderr, err)
-					os.Exit(1)
-				}
-			}
+			getOpenIssues(args, origin)
 		} else if len(args) == 2 {
 			if _, err := strconv.Atoi(args[1]); err == nil {
-				// Get issue information by number
-				result, err := queryObject(
-					"GET",
-					"https://api.github.com/repos/"+origin+"/issues/"+args[1])
-				if err != nil {
-					fmt.Fprintln(os.Stderr, err)
-					os.Exit(1)
-				}
-				if result["pull_request"] != nil {
-					fmt.Fprintln(os.Stderr, args[1]+" is a pull request")
-					os.Exit(1)
-				}
-				link := result["html_url"].(string)
-				splittedLink := strings.Split(link, "/")
-				state := result["state"].(string)
-				if state == "open" {
-					state = "\033[32m" + state + "\033[0m"
-				} else if state == "closed" {
-					state = "\033[31m" + state + "\033[0m"
-				}
-				labels := [](string){}
-				for _, label := range result["labels"].([]interface{}) {
-					labels = append(labels, label.(map[string]interface{})["name"].(string))
-				}
-				if splittedLink[len(splittedLink)-1] == args[1] {
-					fmt.Println("\033[1m" + result["title"].(string) +
-						" [" + state + "]\033[0m")
-					if len(labels) > 0 {
-						fmt.Println("Labels: " + strings.Join(labels[:], ", "))
-					}
-					fmt.Println(result["body"].(string))
-				}
+				getIssueByNumber(args, origin)
 			} else {
 				fmt.Fprintln(os.Stderr, "Unknown arguments for "+args[0])
 				os.Exit(1)
