62f0fd48e235f078d0df62768bda3488e92877ca
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-29 12:57:22 +0300

    (#16) Introduce commit.go

--- main.go
+++ main.go
@@ -29,32 +29,7 @@ func process(args []string) {
 		getInfo(args, origin)
 	case "commit":
 		if len(args) == 1 {
-			// Get list of opened commits
-			fmt.Println("List of commits for " + origin + ":")
-			for page := 1; ; page++ {
-				resp, err := queryList(
-					"GET",
-					"https://api.github.com/repos/"+origin+"/commits?per_page=100&page="+
-						strconv.Itoa(page))
-				if err != nil {
-					fmt.Fprintln(os.Stderr, err)
-					os.Exit(1)
-				}
-				if len(resp) == 0 {
-					break
-				}
-				for _, commit := range resp {
-					fmt.Printf("%s %-*.*s %s\n",
-						commit["sha"].(string)[0:7],
-						20, 20,
-						commit["commit"].(map[string]interface{})["author"].(map[string]interface{})["name"].(string),
-						strings.Split(commit["commit"].(map[string]interface{})["message"].(string), "\n")[0])
-				}
-				if err != nil {
-					fmt.Fprintln(os.Stderr, err)
-					os.Exit(1)
-				}
-			}
+			getOpenCommits(args, origin)
 		} else {
 			fmt.Fprintln(os.Stderr, "Unknown arguments for "+args[0])
 			os.Exit(1)
