633afa3cdf89aacd3b3e5cc74b43f2169d61f707
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-29 13:03:01 +0300

    (#16) Introduce pr.go

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
 func process(args []string) {
@@ -50,67 +49,10 @@ func process(args []string) {
 		}
 	case "pr":
 		if len(args) == 1 {
-			fmt.Println("List of opened pull requests for " + origin + ":")
-			for page := 1; ; page++ {
-				resp, err := queryList(
-					"GET",
-					"https://api.github.com/repos/"+origin+"/issues?per_page=100&page="+
-						strconv.Itoa(page))
-				if err != nil {
-					fmt.Fprintln(os.Stderr, err)
-					os.Exit(1)
-				}
-				if len(resp) == 0 {
-					break
-				}
-				for _, issue := range resp {
-					link := issue["html_url"].(string)
-					if strings.Contains(link, "pull") {
-						fmt.Printf("%-*.*s > %s\n", 50, 50, issue["title"].(string), link)
-					}
-				}
-				if err != nil {
-					fmt.Fprintln(os.Stderr, err)
-					os.Exit(1)
-				}
-			}
+			getOpenPullRequests(args, origin)
 		} else if len(args) == 2 {
 			if _, err := strconv.Atoi(args[1]); err == nil {
-				// Get issue information by number
-				result, err := queryObject(
-					"GET",
-					"https://api.github.com/repos/"+origin+"/pulls/"+args[1])
-				if err != nil {
-					fmt.Fprintln(os.Stderr, err)
-					os.Exit(1)
-				}
-				if result["message"] == "Not Found" {
-					fmt.Fprintln(os.Stderr, args[1]+" is an issue")
-					os.Exit(1)
-				}
-				link := result["html_url"].(string)
-				splittedLink := strings.Split(link, "/")
-				state := result["state"].(string)
-				if state == "open" {
-					state = "\033[32m" + state + "\033[0m"
-				} else if state == "closed" {
-					state = "\033[31m" + state + "\033[0m"
-					if result["merged_at"] != nil {
-						state = "\033[35mmerged\033[0m"
-					}
-				}
-				labels := [](string){}
-				for _, label := range result["labels"].([]interface{}) {
-					labels = append(labels, label.(map[string]interface{})["name"].(string))
-				}
-				if splittedLink[len(splittedLink)-1] == args[1] {
-					fmt.Println("\033[1m" + result["title"].(string) +
-						" [" + state + "]\033[0m")
-					if len(labels) > 0 {
-						fmt.Println("Labels: " + strings.Join(labels[:], ", "))
-					}
-					fmt.Println(result["body"].(string))
-				}
+				getPullRequestByNumber(args, origin)
 			} else {
 				fmt.Fprintln(os.Stderr, "Unknown arguments for "+args[0])
 				os.Exit(1)
---
+++ pr.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getOpenPullRequests(args []string, origin string) {
+	fmt.Println("List of opened pull requests for " + origin + ":")
+	for page := 1; ; page++ {
+		resp, err := queryList(
+			"GET",
+			"https://api.github.com/repos/"+origin+"/issues?per_page=100&page="+
+				strconv.Itoa(page))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		if len(resp) == 0 {
+			break
+		}
+		for _, issue := range resp {
+			link := issue["html_url"].(string)
+			if strings.Contains(link, "pull") {
+				fmt.Printf("%-*.*s > %s\n", 50, 50, issue["title"].(string), link)
+			}
+		}
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+}
+
+func getPullRequestByNumber(args []string, origin string) {
+	result, err := queryObject(
+		"GET",
+		"https://api.github.com/repos/"+origin+"/pulls/"+args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if result["message"] == "Not Found" {
+		fmt.Fprintln(os.Stderr, args[1]+" is an issue")
+		os.Exit(1)
+	}
+	link := result["html_url"].(string)
+	splittedLink := strings.Split(link, "/")
+	state := result["state"].(string)
+	if state == "open" {
+		state = "\033[32m" + state + "\033[0m"
+	} else if state == "closed" {
+		state = "\033[31m" + state + "\033[0m"
+		if result["merged_at"] != nil {
+			state = "\033[35mmerged\033[0m"
+		}
+	}
+	labels := [](string){}
+	for _, label := range result["labels"].([]interface{}) {
+		labels = append(labels, label.(map[string]interface{})["name"].(string))
+	}
+	if splittedLink[len(splittedLink)-1] == args[1] {
+		fmt.Println("\033[1m" + result["title"].(string) +
+			" [" + state + "]\033[0m")
+		if len(labels) > 0 {
+			fmt.Println("Labels: " + strings.Join(labels[:], ", "))
+		}
+		fmt.Println(result["body"].(string))
+	}
+}
