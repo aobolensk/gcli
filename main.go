8384248b2985d713cdc77e5d6838a2856d0ccdbe
Arseniy Obolenskiy <gooddoog@student.su>
2019-12-29 12:10:28 +0300

    (#14) Add 'commit' subcommand
    close #14

--- main.go
+++ main.go
@@ -41,6 +41,40 @@ func process(args []string) {
 			uint(resp["subscribers_count"].(float64)))
 		fmt.Println("Owner: " + resp["owner"].(map[string]interface{})["html_url"].(string))
 		fmt.Println("Last update: " + resp["updated_at"].(string))
+	case "commit":
+		if len(args) == 1 {
+			// Get list of opened commits
+			fmt.Println("List of commits for " + origin + ":")
+			var result []map[string]interface{}
+			for page := 1; ; page++ {
+				resp, err := queryList(
+					"GET",
+					"https://api.github.com/repos/"+origin+"/commits?per_page=100&page="+
+						strconv.Itoa(page))
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+					os.Exit(1)
+				}
+				if len(resp) == 0 {
+					break
+				}
+				result = append(result, resp...)
+			}
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			for _, commit := range result {
+				fmt.Printf("%s %-*.*s %s\n",
+					commit["sha"].(string)[0:7],
+					20, 20,
+					commit["commit"].(map[string]interface{})["author"].(map[string]interface{})["name"].(string),
+					strings.Split(commit["commit"].(map[string]interface{})["message"].(string), "\n")[0])
+			}
+		} else {
+			fmt.Fprintln(os.Stderr, "Unknown arguments for "+args[0])
+			os.Exit(1)
+		}
 	case "issue":
 		if len(args) == 1 {
 			// Get list of opened issues
